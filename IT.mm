ae491df51f48aaa1fd4c408da837736a6b2cefad
Hellseher <sharlatanus@gmail.com>
2015-04-09 23:29:59 +0100

    _a general mm

---
+++ IT.mm
@@ -0,0 +1,28 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1428618497412" ID="ID_867702633" MODIFIED="1428618525398" TEXT="IT">
+<node CREATED="1428618525376" ID="ID_1044255793" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/bash.mm" MODIFIED="1428618525377" POSITION="right" TEXT="bash.mm"/>
+<node CREATED="1428618525378" ID="ID_1376675902" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/cs50.mm" MODIFIED="1428618525378" POSITION="left" TEXT="cs50.mm"/>
+<node CREATED="1428618525378" ID="ID_1846740845" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/networks.mm" MODIFIED="1428618525379" POSITION="right" TEXT="networks.mm"/>
+<node CREATED="1428618525379" ID="ID_1752783658" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/linux.mm" MODIFIED="1428618525380" POSITION="left" TEXT="linux.mm"/>
+<node CREATED="1428618525381" ID="ID_1952037369" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/img_formats.mm" MODIFIED="1428618525381" POSITION="right" TEXT="img_formats.mm"/>
+<node CREATED="1428618525382" ID="ID_1512781975" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/clang.mm" MODIFIED="1428618525382" POSITION="left" TEXT="clang.mm"/>
+<node CREATED="1428618525383" ID="ID_1498447369" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/python.mm" MODIFIED="1428618525383" POSITION="right" TEXT="python.mm"/>
+<node CREATED="1428618525383" ID="ID_1429546206" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/html.mm" MODIFIED="1428618525384" POSITION="left" TEXT="html.mm"/>
+<node CREATED="1428618525384" ID="ID_214874483" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/common-lisp.mm" MODIFIED="1428618525385" POSITION="right" TEXT="common-lisp.mm"/>
+<node CREATED="1428618525385" ID="ID_372441982" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/gui.mm" MODIFIED="1428618525386" POSITION="left" TEXT="gui.mm"/>
+<node CREATED="1428618525386" ID="ID_1565465221" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/emacs.mm" MODIFIED="1428618525387" POSITION="right" TEXT="emacs.mm"/>
+<node CREATED="1428618525387" ID="ID_1469460465" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/vim.mm" MODIFIED="1428618525388" POSITION="left" TEXT="vim.mm"/>
+<node CREATED="1428618525388" ID="ID_1842498044" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/arch.mm" MODIFIED="1428618525389" POSITION="right" TEXT="arch.mm"/>
+<node CREATED="1428618525389" ID="ID_204980964" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/programming.mm" MODIFIED="1428618525390" POSITION="left" TEXT="programming.mm"/>
+<node CREATED="1428618525390" ID="ID_1029436804" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/language.mm" MODIFIED="1428618525391" POSITION="right" TEXT="language.mm"/>
+<node CREATED="1428618525391" ID="ID_1451305991" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/os.mm" MODIFIED="1428618525392" POSITION="left" TEXT="os.mm"/>
+<node CREATED="1428618525392" ID="ID_65187565" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/theshell.mm" MODIFIED="1428618525392" POSITION="right" TEXT="theshell.mm"/>
+<node CREATED="1428618525393" ID="ID_343640133" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/www.mm" MODIFIED="1428618525393" POSITION="left" TEXT="www.mm"/>
+<node CREATED="1428618525394" ID="ID_1557718697" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/algorithms.mm" MODIFIED="1428618525394" POSITION="right" TEXT="algorithms.mm"/>
+<node CREATED="1428618525394" ID="ID_390982578" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/sh.mm" MODIFIED="1428618525395" POSITION="left" TEXT="sh.mm"/>
+<node CREATED="1428618525395" ID="ID_21131033" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/php.mm" MODIFIED="1428618525396" POSITION="right" TEXT="php.mm"/>
+<node CREATED="1428618525396" ID="ID_733167435" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/protocols.mm" MODIFIED="1428618525397" POSITION="left" TEXT="protocols.mm"/>
+<node CREATED="1428618525397" ID="ID_1207321174" LINK="file:/home/sharlatan/Projects/my-GitHub/IT-mm/maps/text_editors.mm" MODIFIED="1428618525398" POSITION="right" TEXT="text_editors.mm"/>
+</node>
+</map>
