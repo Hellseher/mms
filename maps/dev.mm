08221dbea9e0b0e65e6fa1eea4d7931e1d566004
Hellseher <sharlatanus@gmail.com>
2015-07-24 06:58:40 +0100

    _a *.png x 3; _e dev.mm

--- maps/dev.mm
+++ maps/dev.mm
@@ -17,11 +17,88 @@
 <node CREATED="1434742685230" ID="ID_298673546" MODIFIED="1434742687657" POSITION="right" TEXT="DevOps"/>
 <node CREATED="1434742689598" ID="ID_826878119" MODIFIED="1434742692713" POSITION="right" TEXT="Deploy"/>
 <node CREATED="1434742694286" ID="ID_720197898" MODIFIED="1434742702713" POSITION="right" TEXT="Aigle"/>
-<node CREATED="1435339829277" ID="ID_1297452362" MODIFIED="1435339836351" POSITION="left" TEXT="Version Control">
+<node CREATED="1435339829277" ID="ID_1297452362" MODIFIED="1437717100720" POSITION="left" TEXT="Revision Control">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1435339837037" ID="ID_1644452748" MODIFIED="1435339839242" TEXT="git"/>
-<node CREATED="1435339839741" ID="ID_53607356" MODIFIED="1435339841754" TEXT="svn"/>
-<node CREATED="1435339842365" ID="ID_746356875" MODIFIED="1435339845594" TEXT="Mercur"/>
+<node CREATED="1435339837037" ID="ID_1644452748" LINK="https://git-scm.com/" MODIFIED="1437716417195">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>git </b><img src="../icons/git.png" />
+    </p>
+    <p>
+      <font size="3">2005</font><font size="2">&#160;</font>
+    </p>
+    <p>
+      <i><font size="2" color="#3296fa">Linus Torvalds</font></i>
+    </p>
+  </body>
+</html>
+</richcontent>
 </node>
+<node CREATED="1435339839741" ID="ID_53607356" LINK="https://subversion.apache.org/" MODIFIED="1437717122703">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Subversion </b><img src="../icons/svn.png" />
+    </p>
+    <p>
+      2000
+    </p>
+    <p>
+      <font size="2">[ </font><font size="2" color="#3296fa">CollabNet</font><font size="2">&#160;]</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1435339842365" ID="ID_746356875" LINK="https://mercurial.selenic.com/" MODIFIED="1437717054998">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Mercurial </b><img src="../icons/mercurial.png" />
+    </p>
+    <p>
+      2005
+    </p>
+    <p>
+      <i><font size="2" color="#3296fa">Matt Mackall</font></i>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1437716503028" ID="ID_89917847" LINK="http://bazaar.canonical.com/en/" MODIFIED="1437717322434">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>GNU Bazaar </b><img src="../icons/gnu_bazaar.png" />
+    </p>
+    <p>
+      2005
+    </p>
+    <p>
+      <i><font size="2" color="#3296fa">Martin Pool</font></i>
+    </p>
+    <p>
+      <font size="2" color="#000000">[ </font><font size="2" color="#3296fa">Canonical</font><font size="2" color="#000000">&#160;]</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1437717202650" ID="ID_1059632940" MODIFIED="1437717207126" POSITION="left" TEXT="version control"/>
 </node>
 </map>
