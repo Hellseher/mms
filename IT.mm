93a8d25769addd8368449ae02d1cc70c12bef4c1
Hellseher <sharlatanus@gmail.com>
2015-08-21 23:12:46 +0100

    [E] update nodes

--- IT.mm
+++ IT.mm
@@ -1,30 +1,31 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1431365452764" ID="ID_1921554392" MODIFIED="1435271306149" TEXT="IT">
-<node CREATED="1435271306068" ID="ID_1132533900" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/language.mm" MODIFIED="1435271306070" POSITION="right" TEXT="language.mm"/>
-<node CREATED="1435271306070" ID="ID_498478344" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/networks.mm" MODIFIED="1435271306071" POSITION="left" TEXT="networks.mm"/>
-<node CREATED="1435271306072" ID="ID_1050414575" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/vim.mm" MODIFIED="1435271306073" POSITION="right" TEXT="vim.mm"/>
-<node CREATED="1435271306074" ID="ID_507155582" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/img.mm" MODIFIED="1435271306076" POSITION="left" TEXT="img.mm"/>
-<node CREATED="1435271306077" ID="ID_478528468" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/w3cx.mm" MODIFIED="1435271306091" POSITION="right" TEXT="w3cx.mm"/>
-<node CREATED="1435271306092" ID="ID_7515965" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/servers.mm" MODIFIED="1435271306093" POSITION="left" TEXT="servers.mm"/>
-<node CREATED="1435271306094" ID="ID_147201936" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/editors.mm" MODIFIED="1435271306095" POSITION="right" TEXT="editors.mm"/>
-<node CREATED="1435271306103" ID="ID_1738024795" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/python.mm" MODIFIED="1435271306104" POSITION="left" TEXT="python.mm"/>
-<node CREATED="1435271306104" ID="ID_486305829" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/html.mm" MODIFIED="1435271306105" POSITION="right" TEXT="html.mm"/>
-<node CREATED="1435271306106" ID="ID_900480378" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/www.mm" MODIFIED="1435271306107" POSITION="left" TEXT="www.mm"/>
-<node CREATED="1435271306108" ID="ID_1048051499" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/os.mm" MODIFIED="1435271306110" POSITION="right" TEXT="os.mm"/>
-<node CREATED="1435271306110" ID="ID_1780750795" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/cli.mm" MODIFIED="1435271306111" POSITION="left" TEXT="cli.mm"/>
-<node CREATED="1435271306112" ID="ID_308432775" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/grub.mm" MODIFIED="1435271306113" POSITION="right" TEXT="grub.mm"/>
-<node CREATED="1435271306114" ID="ID_556026016" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/arch.mm" MODIFIED="1435271306115" POSITION="left" TEXT="arch.mm"/>
-<node CREATED="1435271306116" ID="ID_1302445041" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/cs.mm" MODIFIED="1435271306117" POSITION="right" TEXT="cs.mm"/>
-<node CREATED="1435271306117" ID="ID_1784856748" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/php.mm" MODIFIED="1435271306118" POSITION="left" TEXT="php.mm"/>
-<node CREATED="1435271306119" ID="ID_1558817607" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/cl.mm" MODIFIED="1435271306136" POSITION="right" TEXT="cl.mm"/>
-<node CREATED="1435271306137" ID="ID_1752726017" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/cs50.mm" MODIFIED="1435271306138" POSITION="left" TEXT="cs50.mm"/>
-<node CREATED="1435271306138" ID="ID_853272911" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/gui.mm" MODIFIED="1435271306139" POSITION="right" TEXT="gui.mm"/>
-<node CREATED="1435271306140" ID="ID_202505548" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/bash.mm" MODIFIED="1435271306141" POSITION="left" TEXT="bash.mm"/>
-<node CREATED="1435271306141" ID="ID_384128515" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/db.mm" MODIFIED="1435271306142" POSITION="right" TEXT="db.mm"/>
-<node CREATED="1435271306143" ID="ID_1929268682" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/c.mm" MODIFIED="1435271306144" POSITION="left" TEXT="c.mm"/>
-<node CREATED="1435271306145" ID="ID_1635343374" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/dev.mm" MODIFIED="1435271306146" POSITION="right" TEXT="dev.mm"/>
-<node CREATED="1435271306146" ID="ID_308350129" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/linux.mm" MODIFIED="1435271306147" POSITION="left" TEXT="linux.mm"/>
-<node CREATED="1435271306148" ID="ID_56949814" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/javascript.mm" MODIFIED="1435271306149" POSITION="right" TEXT="javascript.mm"/>
+<node CREATED="1431365452764" ID="ID_1921554392" MODIFIED="1439673812808" TEXT="IT">
+<node CREATED="1439673812796" ID="ID_929019123" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/arch.mm" MODIFIED="1439673812796" POSITION="left" TEXT="arch.mm"/>
+<node CREATED="1439673812802" ID="ID_1671960629" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/bash.mm" MODIFIED="1439673812802" POSITION="left" TEXT="bash.mm"/>
+<node CREATED="1439673812803" ID="ID_1375427399" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/c.mm" MODIFIED="1439673812804" POSITION="left" TEXT="c.mm"/>
+<node CREATED="1439673812799" ID="ID_1308097349" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/cl.mm" MODIFIED="1439673812799" POSITION="right" TEXT="cl.mm"/>
+<node CREATED="1439673812794" ID="ID_499354392" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/cli.mm" MODIFIED="1439673812794" POSITION="left" TEXT="cli.mm"/>
+<node CREATED="1439673812797" ID="ID_245776777" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/cs.mm" MODIFIED="1439673812797" POSITION="right" TEXT="cs.mm"/>
+<node CREATED="1439673812800" ID="ID_93727627" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/cs50.mm" MODIFIED="1439673812800" POSITION="left" TEXT="cs50.mm"/>
+<node CREATED="1439673812807" ID="ID_126162155" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/css.mm" MODIFIED="1439673812807" POSITION="left" TEXT="css.mm"/>
+<node CREATED="1439673812803" ID="ID_1825239022" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/db.mm" MODIFIED="1439673812803" POSITION="right" TEXT="db.mm"/>
+<node CREATED="1439673812804" ID="ID_839910720" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/dev.mm" MODIFIED="1439673812805" POSITION="right" TEXT="dev.mm"/>
+<node CREATED="1439673812789" ID="ID_229886246" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/editors.mm" MODIFIED="1439673812790" POSITION="right" TEXT="editors.mm"/>
+<node CREATED="1439673812795" ID="ID_1637057585" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/grub.mm" MODIFIED="1439673812795" POSITION="right" TEXT="grub.mm"/>
+<node CREATED="1439673812801" ID="ID_32482867" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/gui.mm" MODIFIED="1439673812801" POSITION="right" TEXT="gui.mm"/>
+<node CREATED="1439673812791" ID="ID_1798476263" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/html.mm" MODIFIED="1439673812792" POSITION="right" TEXT="html.mm"/>
+<node CREATED="1439673812786" ID="ID_1031653764" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/img.mm" MODIFIED="1439673812787" POSITION="left" TEXT="img.mm"/>
+<node CREATED="1439673812806" ID="ID_127616987" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/javascript.mm" MODIFIED="1439673812806" POSITION="right" TEXT="javascript.mm"/>
+<node CREATED="1439673812782" ID="ID_1964691781" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/language.mm" MODIFIED="1439673812784" POSITION="right" TEXT="language.mm"/>
+<node CREATED="1439673812805" ID="ID_1794505642" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/linux.mm" MODIFIED="1439673812806" POSITION="left" TEXT="linux.mm"/>
+<node CREATED="1439673812785" ID="ID_1497499664" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/networks.mm" MODIFIED="1439673812785" POSITION="left" TEXT="networks.mm"/>
+<node CREATED="1439673812793" ID="ID_875922770" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/os.mm" MODIFIED="1439673812793" POSITION="right" TEXT="os.mm"/>
+<node CREATED="1439673812798" ID="ID_1767989812" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/php.mm" MODIFIED="1439673812798" POSITION="left" TEXT="php.mm"/>
+<node CREATED="1439673812790" ID="ID_760092375" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/python.mm" MODIFIED="1439673812791" POSITION="left" TEXT="python.mm"/>
+<node CREATED="1439673812788" ID="ID_1942946374" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/servers.mm" MODIFIED="1439673812789" POSITION="left" TEXT="servers.mm"/>
+<node CREATED="1439673812786" ID="ID_766710009" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/vim.mm" MODIFIED="1439673812786" POSITION="right" TEXT="vim.mm"/>
+<node CREATED="1439673812787" ID="ID_746584939" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/w3cx.mm" MODIFIED="1439673812788" POSITION="right" TEXT="w3cx.mm"/>
+<node CREATED="1439673812792" ID="ID_541604143" LINK="file:/home/sharlaran/Projects/my-GitHub/IT-mm/maps/www.mm" MODIFIED="1439673812792" POSITION="left" TEXT="www.mm"/>
 </node>
 </map>
