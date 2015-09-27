63c1176dd8bdfee08310f9c774177af9189387b7
Hellseher <sharlatanus@gmail.com>
2015-09-27 17:15:03 +0100

    [A] new map     new file:   maps/cyber-security.mm

---
+++ maps/cyber-security.mm
@@ -0,0 +1,23 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1440830385649" ID="ID_623231775" MODIFIED="1440830403391" TEXT="Cyber security">
+<node CREATED="1440830405070" ID="ID_475676873" MODIFIED="1440830407802" POSITION="right" TEXT="Atacks">
+<node CREATED="1440830408789" ID="ID_43789767" MODIFIED="1440830441098">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>MITM</b>
+    </p>
+    <p>
+      <font size="2">man in the middle</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+</map>
