2643d82507b0fbacc419ca8d1ad358ae9981ae79
Hellseher <sharlatanus@gmail.com>
2016-02-18 02:25:17 +0000

    [A] new icon ans map;

---
+++ maps/data-storage.mm
@@ -0,0 +1,72 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1453591737191" ID="ID_599071575" MODIFIED="1453591837884" TEXT="data storage">
+<node CREATED="1453591840014" ID="ID_609695125" MODIFIED="1453591845443" POSITION="right" TEXT="Partitioning">
+<node CREATED="1453591851462" ID="ID_1089475927" MODIFIED="1453591858107" TEXT="Partition table">
+<node CREATED="1453591865749" ID="ID_808608744" MODIFIED="1453591867322" TEXT="MBR"/>
+<node CREATED="1453592555611" ID="ID_1757494634" MODIFIED="1453592558408" TEXT="EBR"/>
+<node CREATED="1453592559274" ID="ID_1308656445" MODIFIED="1453592561160" TEXT="GPT"/>
+<node CREATED="1453592561483" ID="ID_155333174" MODIFIED="1453592563024" TEXT="BEER"/>
+<node CREATED="1453592563538" ID="ID_1166217864" MODIFIED="1453592565072" TEXT="APM"/>
+<node CREATED="1453592565459" ID="ID_1393073915" MODIFIED="1453592566616" TEXT="RDB"/>
+<node CREATED="1453592572331" ID="ID_46698063" MODIFIED="1453592576376" TEXT="BSD disklabel"/>
+</node>
+</node>
+<node CREATED="1453591878141" ID="ID_928603818" MODIFIED="1453591882043" POSITION="right" TEXT="multyboot"/>
+<node CREATED="1453591845894" ID="ID_1223547197" MODIFIED="1453591851011" POSITION="right" TEXT="File systems">
+<node CREATED="1454015286568" ID="ID_521961519" MODIFIED="1454015305831" TEXT="Linux">
+<node CREATED="1454015291778" ID="ID_697107328" MODIFIED="1454015677818">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>ext2</b>
+    </p>
+    <p>
+      1993
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1454015295122" ID="ID_511703358" MODIFIED="1454015654562">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>ext3</b>
+    </p>
+    <p>
+      2001
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1454015297450" ID="ID_1194562374" MODIFIED="1454015481421">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>ext4</b>
+    </p>
+    <p>
+      2006
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1454015833025" ID="ID_559362446" MODIFIED="1454015836399" TEXT="block size"/>
+<node CREATED="1454015836777" ID="ID_804727394" MODIFIED="1454015838574" TEXT="inode"/>
+<node CREATED="1454016056189" ID="ID_1606033890" MODIFIED="1454016061899" TEXT="symbolic link"/>
+</node>
+</node>
+</map>
